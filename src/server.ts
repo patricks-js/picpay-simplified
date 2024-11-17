0d1785672174d14345322f5da247837ec31499f1
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-11-16 22:00:36 -0300

    feat: setup fastify as http framework

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+import { app } from "./app";
+
+try {
+  await app.listen({port: 3333, host: "0.0.0.0"})
+} catch (error) {
+  console.error(error);
+  process.exit(1)
+}
