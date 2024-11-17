46128b292d123e3aa08ab715dfc845c3a3cc5f34
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-11-16 21:58:04 -0300

    feat: setup typescript with tsx for dev mode and tsup for build

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/server.ts"],
+  format: ["esm"],
+  target: "es2022",
+  clean: true,
+  splitting: false,
+  skipNodeModulesBundle: true,
+});
