cf2195eea7f7bcccf9b9307fecc9c4452750c0da
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-11-19 13:52:34 -0300

    refactor(UserRepository): change param name of findByDocument

--- src/domain/repositories/user-repository.ts
+++ src/domain/repositories/user-repository.ts
@@ -8,5 +8,5 @@ import type { IRepository } from "@/domain/interfaces/repository";
 export interface IUserRepository
   extends IRepository<User, string, CreateUserParams, UpdateUserParams> {
   findByEmail(email: string): Promise<User | null>;
-  findByDocument(email: string): Promise<User | null>;
+  findByDocument(document: string): Promise<User | null>;
 }
