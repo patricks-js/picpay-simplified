88ba82702e4c3d7c0472e73cc1f72c11a5851844
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-11-19 14:27:29 -0300

    test: ensure that the user's password is encrypted

--- src/application/use-cases/register-user.spec.ts
+++ src/application/use-cases/register-user.spec.ts
@@ -1,4 +1,5 @@
-import { beforeEach, describe, expect, it } from "vitest";
+import bcrypt from "bcryptjs";
+import { afterAll, beforeEach, describe, expect, it, vi } from "vitest";
 
 import { RegisterUserUseCase } from "@/application/use-cases/register-user";
 import { UserType } from "@/domain/entities/user";
@@ -12,6 +13,10 @@ describe("Use Case -> Register User", () => {
   beforeEach(() => {
     userRepository = new InMemoryUserRepository();
     sut = new RegisterUserUseCase(userRepository);
+  });
+
+  afterAll(() => {
+    vi.resetAllMocks();
   });
 
   it("should be able to register a new user as a customer", async () => {
@@ -59,7 +64,33 @@ describe("Use Case -> Register User", () => {
       }),
     );
   });
-  it.todo("should ensure that user's password is encrypted");
+
+  it("should ensure that user's password is encrypted", async () => {
+    // Arrange
+    const spy = vi.spyOn(bcrypt, "hash");
+    const bcryptRegex = /^\$2[aby]\$.{56}$/;
+    const insertUser = {
+      firstName: "John",
+      surname: "Doe",
+      email: "john.doe@mail.com",
+      document: "12345678911",
+      password: "password",
+      type: UserType.Merchant,
+    };
+
+    // Act
+    const { user } = await sut.execute(insertUser);
+
+    // Assertion
+    expect(user.type).toEqual(UserType.Merchant);
+    expect(user).toEqual(
+      expect.objectContaining({
+        passwordHash: expect.stringMatching(bcryptRegex),
+      }),
+    );
+    expect(spy).toHaveBeenCalledOnce();
+  });
+
   it.todo("should throw an error if user already exists with same email");
   it.todo(
     "should throw an error if user already exists with same document (CPF/CNPJ)",
